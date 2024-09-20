aec80cc61f90ba35b70c9d900ae1ac2162112eab
kaneki2007gooldemon <semenrosev@yandex.ru>
2024-09-20 14:47:17 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+//let a = 2;
+//let b = 3;
+//console.log('a + b = ', a + b); 
+//console.log('a - b = ', a - b); 
+//console.log('a ** 0 =', a ** 0);
+//console.log('b ** 0 =', b ** 0);\
+
+
+
+//let sentence = "раздватри"
+//let len = sentence.length
+//console.log(len)
+
+
+//let number = prompt("Введите число: ");
+
+//if(number > 0) {
+//    alert ("Число положительное")
+//}
+//else if (number <0){
+//    alert ("Число отрицательное")
+//}
+//else{
+//    alert("Число равно нулю")
+//}
+
+
+// let number = +prompt()
+// let number2 = +prompt()
+// let number3 = +prompt()
+// let number4 = +prompt()
+// alert(number + number2 + number3 + number4 / 4)
+
+// let name = prompt('введите имя')
+// let age = prompt('введите возраст')
+// console.log(name,age)
+
+// let number = prompt('введите число')
+// if (number % 2 == 0) {
+//     alert ("Число четное")
+// }
+// else {
+//     alert("Число нечетное")
+// }
+
+// function Number(number) {
+//     if (number < 0) {
+//         console.log("Число отрицательное.");
+//     } else {
+//         console.log("Число не отрицательное.");
+//     }
+// }
+// Number(-5);  
+// Number(3);   
+// Number(0);
+
+
+// let number = prompt("введите число")
+// let number1 = prompt("введите число")
+// if (number % number1 == 0) {
+//     alert('остатка нет')
+// }
+// else {
+//     alert('число с остатком')
+// }
